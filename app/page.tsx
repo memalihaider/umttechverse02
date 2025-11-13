b571b972a0cc783381f041b2e5e9413c4941c6f2
Admin <thenprogrammer@gmail.com>
2025-11-14 01:12:38 +0500

    Add Speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import LandingPage from './components/LandingPage'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
+  <SpeedInsights />
   return <LandingPage />
 }
